e23894fee87fb65731defd83a52cccaa4a6fc061
zxcv124 <53825496+zxcv124@users.noreply.github.com>
2026-04-26 14:02:08 +0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,43 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   context: __dirname,
+
   entry: './frontend/sonusstratos.jsx',
+
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
+
   resolve: {
-    extensions: ['.js', '.jsx', '*']
+    extensions: ['.js', '.jsx']
   },
+
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/env', '@babel/react']
           }
-        },
+        }
       }
     ]
   },
-  devtool: 'source-map'
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './frontend/index.html'
+    })
+  ],
+
+  devtool: 'source-map',
+
+  mode: 'production'
 };
